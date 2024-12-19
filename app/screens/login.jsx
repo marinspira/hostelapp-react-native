2476267b3040f5d5966b08e2b1c96d928e4278b6
Maria Eduarda <102706722+marinspira@users.noreply.github.com>
2024-12-19 00:59:35 +0000

    Create login screen

--- app/screens/login.jsx
+++ app/screens/login.jsx
@@ -1,14 +1,27 @@
 import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 import { useLocalSearchParams } from 'expo-router';
+import SimpleButton from '@/components/buttons/SimpleButton'
+import Logo from '@/assets/images/illustrations/undraw/cabin.svg'
+import { useTranslation } from 'react-i18next';
+import '@/assets/translations/i18n'
+import { Colors } from '@/constants/Colors'
 
 function LoginScreen() {
 
     const { role } = useLocalSearchParams();
+    const { t, i18n } = useTranslation();
 
     return (
         <View style={styles.container}>
-            <Text>Login</Text>
+            <Logo width={120} height={120} />
+            <Text style={styles.title}>Logar como {role === 'guest' ? 'Hóspede' : 'Host'}</Text>
+            <SimpleButton
+                text={role === "guest" ? t("Sou host") : t("Sou hóspede")}
+                onPress={() => router.push("/screens/welcome")}
+                backgroundColor="transparent"
+                textColor={Colors.purple}
+            />
         </View>
     )
 }
@@ -20,5 +33,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    title: {
+        fontFamily: 'PoppinsBold',
+        marginTop: 50,
+        fontSize: 26,
+        marginBottom: 150
     }
 })
