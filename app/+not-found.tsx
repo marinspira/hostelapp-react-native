efc880191af46cd0e238485517194b32f9f23d09
Maria Eduarda <marinspiraa@gmail.com>
2025-06-14 19:06:15 -0300

    Bugfix: log 404 url at not found page

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,14 +1,19 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { usePathname } from 'expo-router';
 
 export default function NotFoundScreen() {
+
+  const pathname = usePathname();
+
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
         <Text >This screen doesn't exist.</Text>
-        <Link href="/guest/(tabs)" style={styles.link}>
+        <Text>Current path: {pathname}</Text>
+        <Link href="/public" style={styles.link}>
           <Text>Go to home screen!</Text>
         </Link>
       </View >
