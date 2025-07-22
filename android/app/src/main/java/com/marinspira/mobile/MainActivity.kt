7ca9cd82d6fdf92c7dfed709a8e669eec6326a62
Maria Eduarda <marinspiraa@gmail.com>
2025-07-22 00:39:13 -0300

    Fix android package

--- android/app/src/main/java/com/marinspira/mobile/MainActivity.kt
+++ android/app/src/main/java/com/marinspira/mobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.marinspira.hostelapp
+package com.marinspira.mobile
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- android/app/src/main/java/com/marinspira/mobile/MainApplication.kt
+++ android/app/src/main/java/com/marinspira/mobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.marinspira.hostelapp
+package com.marinspira.mobile
 
 import android.app.Application
 import android.content.res.Configuration
